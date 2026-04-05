8863132fd2ec62ba27158fef09646eb7444fe341
klownie <audrickyeu@proton.me>
2026-04-05 23:41:13 +0200

    configured nvim better

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -7,21 +7,64 @@
 
       enable = true;
       defaultEditor = true;
+
+      
     
       settings = {
-        vim.viAlias = false;
-        vim.vimAlias = true;
-        vim.lsp.enable = true;
+        vim = {
+          viAlias = false;
+          vimAlias = true;
+          lsp.enable = true;
 
-        vim.languages = {
-          nix.enable = true;
-          rust.enable = true;
-          enableTreesitter = true;
+          undoFile.enable = true;
+
+          options = {
+            tabstop = 2;
+            shiftwidth = 2;
+            expandtab = true;
+            autoindent = true;
+          };
+
+          binds = {
+            cheatsheet.enable = true;
+            whichKey.enable = true;
+          };
+
+          dashboard = {
+            dashboard-nvim.enable = true;
+            alpha.enable = false;
+          };
+
+          languages = {
+            enableFormat = true;
+            enableTreesitter = true;
+            nix.enable = true;
+            rust.enable = true;
+            nu.enable = true;
+          };
+
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          filetree.nvimTree =  {
+            enable = true;
+            openOnSetup = false;
+            setupOpts = {
+              git.enable = true;
+              modified.enable = true;
+            };
+            mappings = {
+              toggle = "e";
+              focus = "o";
+            };
+          };
+          terminal.toggleterm = {
+            enable = true;
+            mappings = {
+              open = "gg";
+            };
+          };
         };
-
-        vim.statusline.lualine.enable = true;
-        vim.telescope.enable = true;
-        vim.autocomplete.nvim-cmp.enable = true;
 
       };
 
